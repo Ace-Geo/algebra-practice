a6ffa86f4f5742c341a30cb95ef5ee6691c5c2b8
Ace-Geo <rgeohud@gmail.com>
2026-04-21 21:34:04 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require("socket.io")(http, {
-  cors: { origin: "*" }
+const http = require('http').createServer();
+const io = require('socket.io')(http, { cors: { origin: "*" } });
+
+io.on('connection', (socket) => {
+    socket.on('join-room', (room) => socket.join(room));
+    socket.on('send-move', (data) => {
+        socket.to(data.roomId).emit('receive-move', data.move);
+    });
 });
 
-// THIS IS THE CRITICAL PART:
-// Use Render's port if available, otherwise use 3000 for local testing
-const PORT = process.env.PORT || 3000;
-
-io.on("connection", (socket) => {
-  console.log("A player connected:", socket.id);
-
-  socket.on("join-room", (roomId) => {
-    socket.join(roomId);
-    console.log(`Player joined room: ${roomId}`);
-  });
-
-  socket.on("send-move", (data) => {
-    socket.to(data.roomId).emit("receive-move", data.move);
-  });
-});
-
-// Tell the server to listen on the correct port
-http.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+http.listen(process.env.PORT || 3000);
